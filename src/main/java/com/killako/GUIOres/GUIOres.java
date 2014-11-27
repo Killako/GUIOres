8b702f42b3a69bb7ee3ef6c93dfdafe67000702e
Killako <shamik97@gmail.com>
2014-11-27 18:21:07 -0500

    e1-4

---
+++ src/main/java/com/killako/GUIOres/GUIOres.java
@@ -0,0 +1,32 @@
+package com.killako.GUIOres;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "GUIOres", name = "GUI Ores", version = "1.7.10-1.0.0")
+public class GUIOres
+{
+    @Mod.Instance("GUIOres")
+    public static GUIOres instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
